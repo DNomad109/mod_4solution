de289666dd84879501f8878e2235a50bb3f07e85
DNomad109 <srob1@mac.com>
2025-07-27 00:50:57 -0400

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,5 +1,14 @@
-// STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
-// See Lecture 52, part 2
+// STEP 2: Wrap the code in an IIFE
+(function (window) {
+  var byeSpeaker = {};
+  var speakWord = "Good Bye";
+
+  byeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  };
+
+  window.byeSpeaker = byeSpeaker;
+})(window);
 
 
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
